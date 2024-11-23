8576e2d217df1572e5709b8c8d3d9257154b13cf
Homelander <karamveersinghisking@gmail.com>
2024-11-23 13:26:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// // window.onload = function() {
+// //    var name = prompt("Write your name below :");
+// //    if (name === null) {
+// //       alert("You did not enter your name.");
+// //   } else if (name.trim() === "") {
+// //       alert("Welcome, Sir!");
+// //   } else {
+// //       alert("Hello, " + name + "!");
+// //   }
+// // }
+
+// var name = prompt("Name :");
+// if (name != null) {
+//     $("h1").text("Welcome " + name + " to Notes-PDF");
+// } else {
+//     $("h1").text("Welcome to Notes-PDF");
+// }
